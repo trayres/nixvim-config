8d33ccb538a6d75013228e6ba775738c00214278
trayres <trayres@gmail.com>
2024-07-13 10:56:11 -0700

    Trying to add DAP plugin.

--- config/plugins.nix
+++ config/plugins.nix
@@ -2,6 +2,157 @@
   plugins = {
     # Buffer bar
     bufferline.enable = true;
+    
+    # Include all treesitter parsers
+    treesitter.enable = true;
+
+    dap = {
+      enable = true;
+      signs = {
+        dapBreakpoint = {
+          text = "●";
+          texthl = "DapBreakpoint";
+        };
+        dapBreakpointCondition = {
+          text = "●";
+          texthl = "DapBreakpointCondition";
+        };
+        dapLogPoint = {
+          text = "◆";
+          texthl = "DapLogPoint";
+        };
+      };
+      extensions = {
+        dap-python = {
+          enable = true;
+        };
+        dap-ui = {
+          enable = true;
+          floating.mappings = {
+            close = ["<ESC>" "q"];
+          };
+        };
+        dap-virtual-text = {
+          enable = true;
+        };
+      }; 
+      configurations = {
+        java = [
+          {
+            type = "java";
+            request = "launch";
+            name = "Debug (Attach) - Remote";
+            hostName = "127.0.0.1";
+            port = 5005;
+          }
+        ];
+      }; 
+    };
+
+    # File tree
+    neo-tree = {
+      enable = true;
+      enableDiagnostics = true;
+      enableGitStatus = true;
+      enableModifiedMarkers = true;
+      enableRefreshOnWrite = true;
+      closeIfLastWindow = true;
+      popupBorderStyle = "rounded"; # Type: null or one of “NC”, “double”, “none”, “rounded”, “shadow”, “single”, “solid” or raw lua code
+      buffers = {
+        bindToCwd = false;
+        followCurrentFile = {
+          enabled = true;
+        };
+      };
+      window = {
+        width = 40;
+        height = 15;
+        autoExpandWidth = false;
+        mappings = {
+          "<space>" = "none";
+        };
+      };
+    };
+
+    # Nix expressions in Neovim
+    nix = {
+      enable = true;
+    };
+
+    # Language server
+    lsp = {
+      enable = true;
+      servers = {
+        # Average webdev LSPs
+        tsserver.enable = true; # TS/JS
+        cssls.enable = true; # CSS
+        tailwindcss.enable = true; # TailwindCSS
+        html.enable = true; # HTML
+        astro.enable = true; # AstroJS
+        #phpactor.enable = true; # PHP
+        svelte.enable = false; # Svelte
+        vuels.enable = false; # Vue
+
+        # Python
+        pyright.enable = true;
+
+        # Markdown
+        marksman.enable = true;
+
+        # Nix
+        nil-ls.enable = true;
+
+        # Docker
+        dockerls.enable = true;
+
+        # Bash
+        bashls.enable = true;
+
+        # C/C++
+        clangd.enable = true;
+
+        # C#
+        csharp-ls.enable = true;
+
+        # Lua
+        lua-ls = {
+          enable = true;
+          settings.telemetry.enable = false;
+        };
+
+        # Rust
+        rust-analyzer = {
+          enable = true;
+          installRustc = true;
+          installCargo = true;
+        };
+      };
+    };
+
+    # Dashboard
+    #alpha = {
+    #  enable = true;
+    #  theme = "dashboard";
+    #  iconsEnabled = true;
+    #};
+
+    cmp.enable = true;
+
+    cmp-nvim-lsp = {
+      enable = true; # Enable suggestions for LSP
+    };
+    cmp-buffer = {
+      enable = true; # Enable suggestions for buffer in current file
+    };
+    cmp-path = {
+      enable = true; # Enable suggestions for file system paths
+    };
+    cmp_luasnip = {
+      enable = true; # Enable suggestions for code snippets
+    };
+    cmp-cmdline = {
+      enable = false; # Enable autocomplete for command line
+    };
   };
-  
+
 }
