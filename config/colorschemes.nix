1c84ee0afa2e11b4536b66a672045a5a474f8bdf
trayres <trayres@gmail.com>
2024-07-21 22:00:45 -0700

    Updating colorscheme

--- config/colorschemes.nix
+++ config/colorschemes.nix
@@ -1,7 +1,44 @@
 {
   colorschemes = {
-    gruvbox = {
+    #gruvbox = {
+    #  enable = true;
+    #};
+    catppuccin = {
       enable = true;
+      settings = {
+        flavour = "mocha";
+        no_bold = false;
+        no_italic = false;
+        no_underline = false;
+        transparent_background = true;
+        integrations = {
+          cmp = true;
+          noice = true;
+          notify = true;
+          neotree = true;
+          harpoon = true;
+          gitsigns = true;
+          which_key = true;
+          illuminate.enabled = true;
+          treesitter = true;
+          treesitter_context = true;
+          telescope.enabled = true;
+          indent_blankline.enabled = true;
+          mini.enabled = true;
+          native_lsp = {
+            enabled = true;
+            inlay_hints = {
+              background = true;
+            };
+            underlines = {
+              errors = ["underline"];
+              hints = ["underline"];
+              information = ["underline"];
+              warnings = ["underline"];
+            };
+          };
+        };
+      };
     };
   };
 }
